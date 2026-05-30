abcfea26edfa89859197e3739d4d20da3afd9fa2
Robert Denham <robert.denham@uq.edu.au>
2026-05-30 10:50:31 +0000

    add optional for un-encrypted files

--- filesender/download.py
+++ filesender/download.py
@@ -1,4 +1,4 @@
-from typing import Iterable, TypedDict
+from typing import Iterable, Optional, TypedDict
 
 from bs4 import BeautifulSoup
 
@@ -6,27 +6,30 @@ from bs4 import BeautifulSoup
 class DownloadFile(TypedDict):
     client_entropy: str
     encrypted: str
-    encrypted_size: int
+    encrypted_size: Optional[int]
     fileaead: str
     fileiv: str
     id: int
     key_salt: str
-    key_version: int
+    key_version: Optional[int]
     mime: str
     #: filename
     name: str
     password_encoding: str
-    password_hash_iterations: int
-    password_version: int
+    password_hash_iterations: Optional[int]
+    password_version: Optional[int]
     size: int
     transferid: int
+
+def _opt_int(value: str) -> Optional[int]:
+    return int(value) if value else None
 
 def files_from_page(content: bytes) -> Iterable[DownloadFile]:
     """
     Yields dictionaries describing the files listed on a FileSender web page
 
     Params:
-        content: The HTML content of the FileSender download page 
+        content: The HTML content of the FileSender download page
     """
     for file in BeautifulSoup(content, "html.parser").find_all(
         class_="file"
@@ -34,17 +37,17 @@ def files_from_page(content: bytes) -> Iterable[DownloadFile]:
         yield {
             "client_entropy": file.attrs[f"data-client-entropy"],
             "encrypted": file.attrs["data-encrypted"],
-            "encrypted_size": int(file.attrs["data-encrypted-size"]),
+            "encrypted_size": _opt_int(file.attrs["data-encrypted-size"]),
             "fileaead": file.attrs["data-fileaead"],
             "fileiv": file.attrs["data-fileiv"],
             "id": int(file.attrs["data-id"]),
             "key_salt": file.attrs["data-key-salt"],
-            "key_version": int(file.attrs["data-key-version"]),
+            "key_version": _opt_int(file.attrs["data-key-version"]),
             "mime": file.attrs["data-mime"],
             "name": file.attrs["data-name"],
             "password_encoding": file.attrs["data-password-encoding"],
-            "password_hash_iterations": int(file.attrs["data-password-hash-iterations"]),
-            "password_version": int(file.attrs["data-password-version"]),
+            "password_hash_iterations": _opt_int(file.attrs["data-password-hash-iterations"]),
+            "password_version": _opt_int(file.attrs["data-password-version"]),
             "size": int(file.attrs["data-size"]),
             "transferid": int(file.attrs["data-transferid"]),
         }
